55e0142fe6ec918e0874de3acea2dfa0a8fb9fdc
Eva <eva.boekhold@gmail.com>
2026-04-20 16:39:30 +0200

    Add simple view at root for weather forecasts

--- Taskr.Client/src/app/app.ts
+++ Taskr.Client/src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,6 +15,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  forecasts = signal<WeatherForecast[]>([]);
+
+  ngOnInit(): void {
+    this.http.get<WeatherForecast[]>('/WeatherForecast')
+      .subscribe(data => this.forecasts.set(data));
+  }
+
   protected readonly title = signal('Taskr.Client');
 }

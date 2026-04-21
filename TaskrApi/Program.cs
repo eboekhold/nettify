83ca0886c6d360414039babdddb8762b258d0590
Eva <eva.boekhold@gmail.com>
2026-04-21 15:44:38 +0200

    Add Swagger UI through Swashbuckle

--- TaskrApi/Program.cs
+++ TaskrApi/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add CORS policy
 builder.Services.AddCors(options =>
 {
@@ -34,6 +37,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();

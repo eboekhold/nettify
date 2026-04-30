a32a591957515f757545d7f92a2e5c316841eeb7
Eva <eva.boekhold@gmail.com>
2026-04-30 19:39:09 +0200

    Show tasks in client

--- TaskrClient/src/app/app.ts
+++ TaskrClient/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+interface Task {
+  id: number;
+  name: string;
+  isComplete: boolean;
 }
 
 @Component({
@@ -17,11 +16,11 @@ interface WeatherForecast {
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
-  forecasts = signal<WeatherForecast[]>([]);
+  tasks = signal<Task[]>([]);
 
   ngOnInit(): void {
-    this.http.get<WeatherForecast[]>('/WeatherForecast')
-      .subscribe(data => this.forecasts.set(data));
+    this.http.get<Task[]>('/api/Tasks')
+      .subscribe(data => this.tasks.set(data));
   }
 
   protected readonly title = signal('TaskrClient');
